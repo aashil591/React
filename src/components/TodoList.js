cb9d462b0e9c1ba39ddd7d655429f7f6ece30dc2
Aashil <ws.aashil@gmail.com>
2021-03-16 16:11:51 +0545

    initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// Component imports.
+import Todo from './Todo';
+
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
+
+    return(
+        <div className="todo-container">
+            <ul className="todo-list">
+                {filteredTodos.map((todo) => (
+                    <Todo
+                        setTodos={setTodos}
+                        todo={todo}
+                        todos={todos}
+                        key={todo.id} 
+                        text={todo.text} 
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default TodoList;
